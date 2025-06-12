934941c6fafbbe5cc56c65ceb460979057b10309
Shruti Sajan <shrutisajan03@gmail.com>
2025-06-12 23:31:30 +0530

    portfolio files uploaded

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: ['animate-blob', 'animation-delay-2000', 'floating-dot'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    'animate-blob',
+    'animation-delay-2000',
+    'floating-dot',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
